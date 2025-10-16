a81854c3e252a6987f040749c63ea5d45c0c3216
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 15:36:48 +0700

    feat : Menambahkan komponen image dengan properti painter (nilainya diambil dari val gambar), contentDescription, dan contentScale di dalam Box 2

--- app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -1,6 +1,7 @@
 package com.example.mylayout_0238
 
 import android.text.Layout
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
@@ -146,6 +148,11 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 .height(height = 300.dp)
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
-        )
+        ) {
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+
+        }
     }
 }
