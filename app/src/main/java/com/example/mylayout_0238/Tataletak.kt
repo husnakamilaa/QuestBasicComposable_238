20a8a3d7e5c55bf63c22fd9aec587c02e75fce82
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 15:42:28 +0700

    fix: memperbaiki gambar dengan gambar terbaru (train)

--- app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -114,7 +114,7 @@ fun TataletakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-    val gambar = painterResource(id = R.drawable.notasibalok)
+    val gambar = painterResource(id = R.drawable.train)
     Column {
         Box(
             modifier = modifier
