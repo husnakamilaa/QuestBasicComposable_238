6a55bff649ea035e8f683a3b4ad7c1dac74df353
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 10:26:02 +0700

    feat : membuat dua function dengan modifikasi tata letak yang berbeda

---
+++ app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -0,0 +1,38 @@
+package com.example.mylayout_0238
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top = 50.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakColumn2(modifier: Modifier) {
+    Column(modifier = Modifier
+        .padding(all = 50.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+
+    {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+
+}
