880613c402689eaa13389a113170e26a52a642a9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 14:41:53 +0700

    feat : menambahkan layout Row 2 (baris 2) didalam layout Column beserta properties komponennya

--- app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -76,5 +76,8 @@ fun TataletakColumnRow(modifier: Modifier) {
         Text(text = "Komponen2Baris1")
         Text(text = "Komponen3Baris1")
     }
+        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+
+        }
     }
 }
