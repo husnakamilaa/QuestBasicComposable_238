e1ca9c6ec90ceb6fbef89fe9d7a8fe89e0446ec8
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 15:20:18 +0700

    feat : menambahkan Spacer 10dp sebagai jarak vertikal antara layout Box pertama dan Box kedua

--- app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -135,6 +136,8 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                     Text(text = "Col1_Row2_Komponen3")
                 }
             }
+
         }
+        Spacer(modifier = Modifier.height(height = 10.dp))
     }
 }
