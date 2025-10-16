cbf989b136846d32a9d0c65bea9b7cbd1e61164e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-10-16 15:46:55 +0700

    fix: menyesuaikan MainActivity untuk menjalankan salah satu function pada file Tataletak

--- app/src/main/java/com/example/mylayout_0238/MainActivity.kt
+++ app/src/main/java/com/example/mylayout_0238/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyLayout_0238Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TataletakBoxColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyLayout_0238Theme {
-        Greeting("Android")
     }
 }
